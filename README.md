4d84e320802fcb65ca702c17b64f3608dd39e10c
anemugomba <53514115+anemugomba@users.noreply.github.com>
2020-03-28 15:36:21 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# angular laravel car rental site and admin
+ An angular and laravel car rental website and admin portal
---
+++ yarutso-laravel/routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::group([
+
+    'middleware' => 'api',
+    /*'namespace' => 'App\Http\Controllers',*/
+    /*'prefix' => 'auth'*/
+
+], function () {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+    Route::post('signup', 'AuthController@signup');
+
+});
